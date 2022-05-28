673e526dc65d6c119e4dd2cc9c5830509fdce575
luguoyn <324883142@qq.com>
2022-05-28 23:06:31 +0800

    初步实现了查找用户收藏商品的功能

--- src/main/java/com/suda/fleamarket/service/StarService.java
+++ src/main/java/com/suda/fleamarket/service/StarService.java
@@ -1,7 +1,10 @@
 package com.suda.fleamarket.service;
 
+import com.suda.fleamarket.entity.Goods;
 import com.suda.fleamarket.entity.Star;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
 
 /**
 * @author 32488
@@ -9,5 +12,10 @@ import com.baomidou.mybatisplus.extension.service.IService;
 * @createDate 2022-05-23 18:58:09
 */
 public interface StarService extends IService<Star> {
+    List<Goods> listAllByUserId(Long userId);
+
+    boolean save(Long userId, Long goodId);
+
+    boolean remove(Long userId, Long goodId);
 
 }
--- src/main/java/com/suda/fleamarket/service/impl/StarServiceImpl.java
+++ src/main/java/com/suda/fleamarket/service/impl/StarServiceImpl.java
@@ -1,11 +1,17 @@
 package com.suda.fleamarket.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.suda.fleamarket.entity.Goods;
 import com.suda.fleamarket.entity.Star;
+import com.suda.fleamarket.mapper.GoodsMapper;
 import com.suda.fleamarket.service.StarService;
 import com.suda.fleamarket.mapper.StarMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @author 32488
@@ -17,6 +23,31 @@ public class StarServiceImpl extends ServiceImpl<StarMapper, Star>
         implements StarService {
     @Autowired
     StarMapper starMapper;
+    @Autowired
+    GoodsMapper goodsMapper;
+
+    @Override
+    public List<Goods> listAllByUserId(Long userId) {
+        List<Long> goodIdList = new LinkedList<>();
+        starMapper.selectList(
+                new LambdaQueryWrapper<Star>()
+                        .select(Star::getGoodId)
+                        .eq(Star::getUserId, userId)
+        ).forEach(star -> goodIdList.add(star.getUserId()));
+        return goodsMapper.selectList(new LambdaQueryWrapper<Goods>().in(Goods::getId, goodIdList));
+    }
+
+    @Override
+    public boolean save(Long userId, Long goodId) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Long userId, Long goodId) {
+        return false;
+    }
+
+
 }
 
 
